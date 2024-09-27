1db7c5f2a2d53ae17adffc2787b55ecedee25bed
Vladyslav Bagatskyy <vladyslavbagatskyy@gmail.com>
2024-09-27 13:44:11 +0100

    made a random number roles generator

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 import os
+import random
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -12,15 +13,43 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Bot is ready. Logged in as {bot.user}')
     try:
-        synced = await bot.tree.sync()  # Sync the slash commands globally
+        synced = await bot.tree.sync() 
         print(f'Synced {len(synced)} commands.')
     except Exception as e:
         print(f'Failed to sync commands: {e}')
 
+
+list_id = 1289151239120097365
+channel_id = 1288962369564708966
+
+
+def generate_idnex():
+    chair_idx = random.randint(0, 5)
+    scribe_idx = random.randint(0, 5)
+
+    if chair_idx == scribe_idx:
+        raise ValueError
+    
+    return (chair_idx, scribe_idx)
+
 @bot.tree.command(name="roles", description="Choose a chair and a scribe")
 async def hello(interaction: discord.Interaction):
-    await interaction.response.send_message(f'hello, {interaction.user.mention}!')
 
+    channel = bot.get_channel(channel_id)
+
+    if channel == None:
+        print("Channel not found")
+
+    list = await channel.fetch_message(list_id)
+    members = list.content
+    members = members.split(',')
+
+    try:
+        chair_idx, scribe_idx = generate_idnex()
+    except ValueError:
+        chair_idx, scribe_idx = generate_idnex()
+
+    await interaction.response.send_message(f'Today chair is {members[chair_idx]} and scribe is {members[scribe_idx]}')
 
 try:
     temp = os.getenv('heroku')
